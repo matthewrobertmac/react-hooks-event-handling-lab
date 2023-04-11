6a473411787a9c795afedd144dec93a93311342f
+ <tidydata@outlook.com>
2023-04-11 14:10:16 -0400

    this should work

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    return <button onFocus={(e) => {(console.log("Good!"))}} onBlur={(e) => {(console.log("Hey! Eyes on me!"))}}>EyesOnMe</button>
+}
+
+export default EyesOnMe
