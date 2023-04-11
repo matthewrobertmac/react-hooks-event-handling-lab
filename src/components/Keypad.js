1f3a43b28324baf38faf8e6f7cd8d5b7e84b0723
+ <tidydata@outlook.com>
2023-04-11 17:18:41 -0400

    should work but some tests still failing

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,8 +2,13 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={(e) => {
+                (console.log('Entering password...'))
+            }}></input>
+        </div>
     )
 }
 
 export default Keypad;
+
